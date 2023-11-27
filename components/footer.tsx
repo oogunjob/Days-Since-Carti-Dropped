533702bd71aa6213a16dacd294aee1e90c59d04e
Tosin Ogunjobi <ogunjobiforex@gmail.com>
2023-11-26 23:49:15 -0600

    fixing footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ function Footer() {
     return (
         <footer className='my-5 font-semibold'>
             <div className="flex flex-col justify-center items-center">
-                <div className='text-center'>Created by Tosin Ogunjobi. Follow me on X <a href="https://x.com/africanxmamba_" target="_blank">@africanxmamba_</a></div>
                 <div className='text-center pt-2'>Inspired by <a href="https://playboicarti.cloud/" target="_blank">Playboi Carti Translator</a></div>
+                <div className='text-center'>Created by <a href="https://x.com/africanxmamba_" target="_blank">@africanxmamba_</a></div>
             </div>
         </footer>
     )
