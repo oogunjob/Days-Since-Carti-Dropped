2602a30a00cb96680127c7b1e624597692e83a43
Tosin Ogunjobi <ogunjobiforex@gmail.com>
2023-11-28 17:35:33 -0800

    fix count up timer

--- components/countuptimer.tsx
+++ components/countuptimer.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 const CountUpTimer = ({ startDate }: { startDate: string }) => {
 
-    const calculateTimeDifference = (startDate: string) => {
+    const CalculateTimeDifference = (startDate: string) => {
         const now = new Date();
         const start = new Date(startDate);
         const diff = now.getTime() - start.getTime();
@@ -20,18 +20,35 @@ const CountUpTimer = ({ startDate }: { startDate: string }) => {
         return { days, hours, minutes, seconds };
     };
 
-    const [timeDiff, setTimeDiff] = useState(calculateTimeDifference(startDate));
+    const [timeDiff, setTimeDiff] = useState<{ days: number, hours: number, minutes: number, seconds: number } | null>(null);
+    const [isClient, setIsClient] = useState<boolean>(false);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTimeDiff(calculateTimeDifference(startDate));
-        }, 1000);
+        // Set to true once component mounts, indicating client-side rendering
+        setIsClient(true);
+    }, []);
 
-        return () => clearInterval(interval);
-    }, [startDate]);
+    useEffect(() => {
+        if (isClient) {
+            // Set the initial time difference immediately, then update every second
+            setTimeDiff(CalculateTimeDifference(startDate));
+
+            const interval = setInterval(() => {
+                setTimeDiff(CalculateTimeDifference(startDate));
+            }, 1000);
+
+            // Clear interval on cleanup
+            return () => clearInterval(interval);
+        }
+    }, [startDate, isClient]);
+
+    if (!isClient || !timeDiff) {
+        // Return null or a loader until the actual date is available
+        return <div>...</div>
+    }
 
     return (
-        <div suppressHydrationWarning={true}>
+        <div>
             {timeDiff.days} DAYS {timeDiff.hours} HOURS {timeDiff.minutes} MINUTES {timeDiff.seconds} SECONDS
         </div>
     );
