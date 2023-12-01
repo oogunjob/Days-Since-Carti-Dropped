8d620853321f424cd909e317bf2d0473522acb51
Tosin Ogunjobi <ogunjobiforex@gmail.com>
2023-11-30 19:55:31 -0800

    originalgraph fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,20 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Days Since Carti Dropped',
-  description: 'Tracking the number of days since recording artist Playboi Carti has dropped an album',
-  keywords: ['Playboi Carti', 'Carti', 'Whole Lotta Red', 'Die Lit', 'Self Titled', 'Days Since', 'Days Since Carti Dropped', 'Days Since Carti Dropped an Album', 'Days Since Carti Dropped Whole Lotta Red', 'Days Since Carti Dropped Die Lit', 'Days Since Carti Dropped Self Titled', 'WLR'],
+  description: 'Tracking the number of days since recording artist Playboi Carti has dropped music',
+  keywords: ['Playboi Carti', 'Carti', 'Whole Lotta Red', 'Die Lit', 'Self Titled', 'Days Since', 'Days Since Carti Dropped', 'Days Since Carti Dropped an Album', 'Days Since Carti Dropped Whole Lotta Red', 'Days Since Carti Dropped Die Lit', 'Days Since Carti Dropped Self Titled', 'WLR', 'How many days', 'days since Carti dropped'],
   authors: [{ name: 'Tosin Ogunjobi' }],
   robots: 'index, follow',
   icons: {
     icon: ['/favicon.ico?v=4'],
     apple: ['/apple-touch-icon.png?v=4'],
     shortcut: ['/apple-touch-icon.png'],
+  },
+  openGraph: {
+    title: 'Days Since Carti Dropped',
+    description: 'Tracking the number of days since recording artist Playboi Carti has dropped music',
+    url: 'https://dayssincecartidropped.com',
+    siteName: 'Days Since Carti Dropped'
   }
 }
 
