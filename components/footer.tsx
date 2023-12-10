cfed8ecb9732b50cf6558678ad11cd30254eea27
Tosin Ogunjobi <ogunjobiforex@gmail.com>
2023-12-09 20:53:30 -0800

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className='my-5 font-semibold'>
             <div className="flex flex-col justify-center items-center">
-                <div className='text-center pt-2'>Inspired by <a href="https://playboicarti.cloud/" target="_blank">Playboi Carti Translator</a></div>
+                <div className='text-center pt-2 text-lg'>Inspired by <a href="https://playboicarti.cloud/" target="_blank">Playboi Carti Translator</a></div>
             </div>
         </footer>
     )
